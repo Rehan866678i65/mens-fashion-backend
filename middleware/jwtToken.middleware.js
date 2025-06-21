69dadd5bc6bb498f0fec727300bbeb7b647b9130
Rehan866678i65 <memonraiyan48@gmail.com>
2025-06-21 21:39:29 +0530

    Update jwtToken.middleware.js

--- middleware/jwtToken.middleware.js
+++ middleware/jwtToken.middleware.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
-require("dotenv").config(); // ये जरूरी है .env पढ़ने के लिए
+require("dotenv").config(); // जरूरी है .env पढ़ने के लिए
 
-const JWT_SECRET = process.env.JWT_SECRET || "fallback";
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
@@ -11,7 +11,7 @@ const verifyToken = (req, res, next) => {
 
   jwt.verify(token, JWT_SECRET, (err, decoded) => {
     if (err) {
-      console.log("JWT verification failed:", err);
+      console.error("JWT verification failed:", err);
       return res.status(403).json({ error: "Invalid token" });
     }
     req.user = decoded;
