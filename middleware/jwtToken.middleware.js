01bc14dc1a1fdacda488510abda949fd6ba4a191
Rehan866678i65 <memonraiyan48@gmail.com>
2025-06-21 21:31:56 +0530

    Update jwtToken.middleware.js

--- middleware/jwtToken.middleware.js
+++ middleware/jwtToken.middleware.js
@@ -1,13 +1,15 @@
 const jwt = require("jsonwebtoken");
- // same secret as in LoginController
+require("dotenv").config(); // ये जरूरी है .env पढ़ने के लिए
+
+const JWT_SECRET = process.env.JWT_SECRET || "fallback";
 
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   if (!authHeader) return res.status(401).json({ error: "No token provided" });
 
-  const token = authHeader.split(" ")[1];  // assuming format "Bearer <token>"
+  const token = authHeader.split(" ")[1];
 
-  jwt.verify(token,  (err, decoded) => {
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
     if (err) {
       console.log("JWT verification failed:", err);
       return res.status(403).json({ error: "Invalid token" });
